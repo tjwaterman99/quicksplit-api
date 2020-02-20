d001439481069c81bc6935b36e95653cff7509e4
Tom Waterman <tjwaterman99@gmail.com>
2020-02-19 18:12:09 -0800

    Try propogate exceptions

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ class ProductionConfig(object):
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    PROPAGATE_EXCEPTIONS = True
 
 
 class TestingConfig(ProductionConfig):
