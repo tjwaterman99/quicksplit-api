691e36e0009462d8ff4ae1aef8c8b85c8915bd96
Tom Waterman <tjwaterman99@gmail.com>
2020-03-04 15:49:08 -0800

    WIP move styles to individual components

--- www/src/main.js
+++ www/src/main.js
@@ -1,10 +1,11 @@
 import './assets/scss/styles.scss'
-import 'bootstrap'
 
 import Vue from 'vue';
+import BootstrapVue from  'bootstrap-vue';
 import App from './App.vue';
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
