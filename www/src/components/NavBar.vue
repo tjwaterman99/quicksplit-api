e0610d4bd961909b6ff6025b2022e36851bec592
Tom Waterman <tjwaterman99@gmail.com>
2020-03-04 12:27:00 -0800

    Move Navbar to its own component

---
+++ www/src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="container-fluid menu">
+		<div class="row">
+			<div class="col-sm-12">
+					<span><a href="#faq">FAQ</a></span>
+					<span><a href="#contact">Contact</a></span>
+					<span><a href="#get-started">Get started</a></span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "NavBar",
+	props: {}
+}
+</script>
+
+<style>
+</style>
