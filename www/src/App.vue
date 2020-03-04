ab86f7061aa09935f90885b8e3a8e74fdebdffa4
Tom Waterman <tjwaterman99@gmail.com>
2020-03-04 12:14:41 -0800

    Move contact to its own component

--- www/src/App.vue
+++ www/src/App.vue
@@ -19,40 +19,40 @@
       </div>
     </div>
 
-<p class="anchor" id="get-started"></p>
-<div class="container">
-<div class="row">
-<div class="col-12 col-lg-8 offset-lg-2">
-<div class="section">
-<h3>Register an account</h3>
-<hr>
-<p>Download the CLI (requires Python3.7+ and pip installed on your system).</p>
-<pre>
-$ pip install --user quicksplit
-</pre>
-<p>Register a new account.</p>
-<pre>
-$ quicksplit register
-</pre>
-<p>You'll be prompted for an email address and password. (We promise never to send you unhelpful marketing emails).</p>
-<p>Create an experiment.</p>
-<pre>
-$ quicksplit create landing-page-test
-</pre>
-<p>You'll use the name of the experiment in the next steps, where you'll log exposure and conversion events for the experiment.<p>
-<p>But before you can log events, you'll need to locate the <i><b>public, staging</b></i> token for your account.</p>
-<pre>
-$ export TOKEN=$(quicksplit tokens --staging --public)
-</pre>
+    <p class="anchor" id="get-started"></p>
+    <div class="container">
+    <div class="row">
+    <div class="col-12 col-lg-8 offset-lg-2">
+    <div class="section">
+    <h3>Register an account</h3>
+    <hr>
+    <p>Download the CLI (requires Python3.7+ and pip installed on your system).</p>
+    <pre>
+    $ pip install --user quicksplit
+    </pre>
+    <p>Register a new account.</p>
+    <pre>
+    $ quicksplit register
+    </pre>
+    <p>You'll be prompted for an email address and password. (We promise never to send you unhelpful marketing emails).</p>
+    <p>Create an experiment.</p>
+    <pre>
+    $ quicksplit create landing-page-test
+    </pre>
+    <p>You'll use the name of the experiment in the next steps, where you'll log exposure and conversion events for the experiment.<p>
+    <p>But before you can log events, you'll need to locate the <i><b>public, staging</b></i> token for your account.</p>
+    <pre>
+    $ export TOKEN=$(quicksplit tokens --staging --public)
+    </pre>
 
-<p><b>Public</b> tokens provide for limited access to your account, only allowing logging events. They should always be used in client-side applications such as a JavaScript web app.</p>
-<p><b>Staging</b> tokens are used to separate staging data from production data. They should be used in your development environment.</p>
-<br>
-<h3>Log an exposure event</h3>
-<hr>
-<p>Log an <i>exposure</i> for your experiment by sending JSON encoded data to the <code>/exposures</code> route.</p>
-<pre>
-$ curl https://api.quicksplit.io/exposures \
+    <p><b>Public</b> tokens provide for limited access to your account, only allowing logging events. They should always be used in client-side applications such as a JavaScript web app.</p>
+    <p><b>Staging</b> tokens are used to separate staging data from production data. They should be used in your development environment.</p>
+    <br>
+    <h3>Log an exposure event</h3>
+    <hr>
+    <p>Log an <i>exposure</i> for your experiment by sending JSON encoded data to the <code>/exposures</code> route.</p>
+    <pre>
+    $ curl https://api.quicksplit.io/exposures \
     -H "Authorization: ${TOKEN}" \
     -H "Content-Type: application/json" \
     -d '{
@@ -60,115 +60,103 @@ $ curl https://api.quicksplit.io/exposures \
       "subject": 12345,
       "cohort": "control"
     }'
-</pre>
-<p>Exposures have three required fields: experiment, subject, and cohort.
-<ul>
-<li><code>experiment</code> is the name of the experiment you created previously. In this case, it's <code>landing-page-test</code></li>
-<li><code>subject</code> is typically a user id provided by your own backend.</li>
-<li><code>cohort</code> corresponds to which version of your application the user is experiencing. In an A/B test, you might use the labels "experimental" and "control" for your cohorts.</li>
-</ul>
-</p>
+    </pre>
+    <p>Exposures have three required fields: experiment, subject, and cohort.
+    <ul>
+    <li><code>experiment</code> is the name of the experiment you created previously. In this case, it's <code>landing-page-test</code></li>
+    <li><code>subject</code> is typically a user id provided by your own backend.</li>
+    <li><code>cohort</code> corresponds to which version of your application the user is experiencing. In an A/B test, you might use the labels "experimental" and "control" for your cohorts.</li>
+    </ul>
+    </p>
 
-<br>
-<h3>Log a conversion event</h3>
-<hr>
-<p>Log a <i>conversion</i> for your experiment by sending JSON encoded data to the <code>/conversions</code> route.</p>
-<pre>
-$ curl https://api.quicksplit.io/conversions \
+    <br>
+    <h3>Log a conversion event</h3>
+    <hr>
+    <p>Log a <i>conversion</i> for your experiment by sending JSON encoded data to the <code>/conversions</code> route.</p>
+    <pre>
+    $ curl https://api.quicksplit.io/conversions \
     -H "Authorization: " \
     -H "Content-Type: application/json" \
     -d '{
       "experiment": "landing-page-test",
       "subject": 12345
     }'
-</pre>
+    </pre>
 
 
-<p>Confirm that the exposure and conversion logging was successful by checking the recent events.</p>
+    <p>Confirm that the exposure and conversion logging was successful by checking the recent events.</p>
 
-<pre>
-$ quicksplit recent --staging
+    <pre>
+    $ quicksplit recent --staging
 
-┌────────────┬───────────────────┬─────────┬─────────┬───────┬───────────────────────────────┐
-│ Type       │ Experiment        │ Subject │ Cohort  │ Value │ Last seen                     │
-├────────────┼───────────────────┼─────────┼─────────┼───────┼───────────────────────────────┤
-│ conversion │ landing-page-test │ 12345   │ control │       │ Tue, 25 Feb 2020 21:37:52 GMT │
-│ exposure   │ landing-page-test │ 12345   │ control │       │ Tue, 25 Feb 2020 21:34:35 GMT │
-└────────────┴───────────────────┴─────────┴─────────┴───────┴───────────────────────────────┘
-</pre>
+    ┌────────────┬───────────────────┬─────────┬─────────┬───────┬───────────────────────────────┐
+    │ Type       │ Experiment        │ Subject │ Cohort  │ Value │ Last seen                     │
+    ├────────────┼───────────────────┼─────────┼─────────┼───────┼───────────────────────────────┤
+    │ conversion │ landing-page-test │ 12345   │ control │       │ Tue, 25 Feb 2020 21:37:52 GMT │
+    │ exposure   │ landing-page-test │ 12345   │ control │       │ Tue, 25 Feb 2020 21:34:35 GMT │
+    └────────────┴───────────────────┴─────────┴─────────┴───────┴───────────────────────────────┘
+    </pre>
 
-<p>Note that we used the special <code>--staging</code> flag to retrieve recent staging events. Omitting that flag would pull recent production events instead.</p>
+    <p>Note that we used the special <code>--staging</code> flag to retrieve recent staging events. Omitting that flag would pull recent production events instead.</p>
 
-<br>
-<h3>Download the results of your experiment</h3>
-<hr>
+    <br>
+    <h3>Download the results of your experiment</h3>
+    <hr>
 
-<p>You can retrieve the statistical significance of your experiment as soon as it has started receiving events.</p>
+    <p>You can retrieve the statistical significance of your experiment as soon as it has started receiving events.</p>
 
-<pre>
-$ quicksplit results landing-page-test --staging
+    <pre>
+    $ quicksplit results landing-page-test --staging
 
-┌─────────┬──────────┬─────────────────┬────────────────────┐
-│ Cohort  │ Subjects │ Conversion rate │ 95% conf. interval │
-├─────────┼──────────┼─────────────────┼────────────────────┤
-│ control │ 1        │ 1               │ [,]                │
-└─────────┴──────────┴─────────────────┴────────────────────┘
-Your test is not statistically significant. Either you need to collect more data, or there is no difference in means between the cohorts
-</pre>
+    ┌─────────┬──────────┬─────────────────┬────────────────────┐
+    │ Cohort  │ Subjects │ Conversion rate │ 95% conf. interval │
+    ├─────────┼──────────┼─────────────────┼────────────────────┤
+    │ control │ 1        │ 1               │ [,]                │
+    └─────────┴──────────┴─────────────────┴────────────────────┘
+    Your test is not statistically significant. Either you need to collect more data, or there is no difference in means between the cohorts
+    </pre>
 
-<p>Note that we're using the <code>--staging</code> flag here again because we've been using the staging token.</p>
+    <p>Note that we're using the <code>--staging</code> flag here again because we've been using the staging token.</p>
 
-</div>
-</div>
-</div>
+    </div>
+    </div>
+    </div>
 
-<p class="anchor" id="faq"></p>
-<div class="row">
-<div class="col-12 col-lg-8 offset-lg-2">
-<div class="section faq">
-<h2 class="text-center">Frequently asked questions</h2>
-<br>
-<p class="faq-question"><b>Q: What sample sizes can I use for a single experiment?</b></p>
-<p>You can log up to <b>3,000 subjects</b> in a single experiment. In typical situations, that's a large enough sample size to detect ~1% differences in conversion rates.<p>
-<hr>
-<p class="faq-question"><b>Q: Can I run multiple experiments at the same time?</b></p>
-<p>You can run up to <b>three concurrent experiments</b> on a single account. To stop an existing experiment, use the command <code>quicksplit stop</code>. To restart an experiment, use the command <code>quicksplit start</code>.<p>
-<hr>
-<p class="faq-question"><b>Q: Can I do a three-way test (an A/B/C test)?</b></p>
-<p>You can use any number of cohorts for your experiment, including running three way tests.<p>
-<hr>
-<p class="faq-question"><b>Q: How do you calculate whether my experiment is statistically significant?</b></p>
-<p>We use the industry-standard methodology called an <b><a href="https://en.wikipedia.org/wiki/Analysis_of_variance">ANOVA</a></b> test. If the F-statistic of the ANOVA test has an associated p-value of less than 0.1, we conclude that the test is significant.<p>
-<hr>
-</div>
-</div>
-</div>
+    <p class="anchor" id="faq"></p>
+    <div class="row">
+    <div class="col-12 col-lg-8 offset-lg-2">
+    <div class="section faq">
+    <h2 class="text-center">Frequently asked questions</h2>
+    <br>
+    <p class="faq-question"><b>Q: What sample sizes can I use for a single experiment?</b></p>
+    <p>You can log up to <b>3,000 subjects</b> in a single experiment. In typical situations, that's a large enough sample size to detect ~1% differences in conversion rates.<p>
+    <hr>
+    <p class="faq-question"><b>Q: Can I run multiple experiments at the same time?</b></p>
+    <p>You can run up to <b>three concurrent experiments</b> on a single account. To stop an existing experiment, use the command <code>quicksplit stop</code>. To restart an experiment, use the command <code>quicksplit start</code>.<p>
+    <hr>
+    <p class="faq-question"><b>Q: Can I do a three-way test (an A/B/C test)?</b></p>
+    <p>You can use any number of cohorts for your experiment, including running three way tests.<p>
+    <hr>
+    <p class="faq-question"><b>Q: How do you calculate whether my experiment is statistically significant?</b></p>
+    <p>We use the industry-standard methodology called an <b><a href="https://en.wikipedia.org/wiki/Analysis_of_variance">ANOVA</a></b> test. If the F-statistic of the ANOVA test has an associated p-value of less than 0.1, we conclude that the test is significant.<p>
+    <hr>
+    </div>
+    </div>
+    </div>
 
-<p class="anchor" id="contact"></p>
-<div class="row">
-<div class="col-12 col-lg-8 offset-lg-2">
-<div class="section">
-<h2 class="text-center">Contact</h2>
-<br>
-<p>You can <a href="mailto:tjwaterman99@gmail.com">email us</a> with any questions or issues. Thanks for trying out <code>quicksplit</code>!</p>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-</div>
-</div>
-</div>
-</div>
-
+  <Contact />
+  </div>
 </div>
 </template>
 
 <script>
+import Contact from './components/Contact'
 
 export default {
   name: 'App',
+  components: {
+    'Contact': Contact
+  }
 }
 </script>
 
