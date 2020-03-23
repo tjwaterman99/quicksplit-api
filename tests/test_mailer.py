d264e8e8767084c28c38b5e6baa0d1c37bcbb8c3
Tom Waterman <tjwaterman99@gmail.com>
2020-03-23 10:40:53 -0700

    Update  test for sandbox usage

--- tests/test_mailer.py
+++ tests/test_mailer.py
@@ -3,7 +3,8 @@ from app.proxies import mailer
 
 def test_mailer_can_send(client, user):
     resp = mailer._send(route="/mail/send", to=user.email, content="test content", subject="test subject")
-    assert resp.status_code == 202
+    # When using sandbox this is 200, when not using sandbox it's  202
+    assert resp.status_code == 200
 
 def test_mailer_can_send_async(client, user):
     job = mailer._send_async(route="/mail/send", to=user.email, content="test content", subject="test subject")
